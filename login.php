0a37971db00b9ecd8ea0e83efeeada8862f543e3
AchmadMahfud26 <achmadmahfud01@gmail.com>
2025-07-27 20:07:25 +0700

    +favicon

--- login.php
+++ login.php
@@ -16,6 +16,7 @@ unset($_SESSION['error']);
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Login Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="icon" type="image/png" href="assets/img/agriculture.png" />
 </head>
 <body class="bg-light d-flex align-items-center justify-content-center" style="height: 100vh;">
     <div class="card p-4 shadow-sm" style="width: 320px; border-radius: 12px;">
--- register.php
+++ register.php
@@ -14,6 +14,7 @@ if (isset($_SESSION['user'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Registrasi Admin Baru</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="icon" type="image/png" href="assets/img/agriculture.png" />
 </head>
 <body class="bg-light d-flex align-items-center justify-content-center" style="height: 100vh;">
     <div class="card p-4 shadow-sm" style="width: 320px; border-radius: 12px;">
